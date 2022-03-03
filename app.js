bb180df2119bdbfcdd782465e1949d3ed6e6151e
bottle-honey <honey_sheep@naver.com>
2022-03-04 01:48:42 +0900

    #4.5
    1.문자열 표현 방법
    2. localStorage API의 사용방법

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+// id,class, 태그로 값을 가져올 수 있음
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 function onLoginSubmit(event){
-    event.preventDefault();
-    loginForm.classList.add("hidden");
+    event.preventDefault();//브라우저 기본 작동 방지
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    gree
-
+    localStorage.setItem("username",username);
+    // 문자열 표현 방법
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 loginForm.addEventListener("submit",onLoginSubmit);
