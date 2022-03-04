f764365ef5c8e65483398bd22d34fdb9ce63506a
bottle-honey <honey_sheep@naver.com>
2022-03-05 01:51:28 +0900

    #4.6

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 function onLoginSubmit(event){
     event.preventDefault();//브라우저 기본 작동 방지
     loginForm.classList.add(HIDDEN_CLASSNAME);
+
     const username = loginInput.value;
-    localStorage.setItem("username",username);
+    localStorage.setItem(USERNAME_KEY,username);
     // 문자열 표현 방법
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(username);
 }
-loginForm.addEventListener("submit",onLoginSubmit);
+
+function paintGreetings(username){
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+}
+else{
+    paintGreetings(savedUsername);
+}
+    
