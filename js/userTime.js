3337ddfb27ce9b71df440c4d77d5168049170324
bottle-honey <honey_sheep@naver.com>
2022-03-17 19:00:59 +0900

    Add userTime.js
    브라우저 사용시간 기능

---
+++ js/userTime.js
@@ -0,0 +1,32 @@
+const USERTIME_KEY = "usertime";
+const userTimeClock = document.querySelector("#userTime");
+
+function SavingUserTime(){
+    const CurrentUserTime = localStorage.getItem(USERTIME_KEY);
+    if (CurrentUserTime === null){
+        localStorage.setItem(USERTIME_KEY,"0");
+    }
+    else{
+        localStorage.setItem(USERTIME_KEY,String(Number(CurrentUserTime)+1));
+    }
+}
+
+function displayUserTime(){
+    const CurrentUserTime = Number(localStorage.getItem(USERTIME_KEY));
+    var hours = parseInt(CurrentTime/3600);
+    var minutes = parseInt((CurrentUserTime - (CurrentTime%3600)) / 60);
+    var seconds = CurrentUserTime % 60;
+    if(isNaN(hours)){
+        hours = 0;
+    }
+    if(isNaN(minutes)){
+        minutes = 0;
+    }
+    userTimeClock.innerText = `${String(hours).padStart(2,"0")}:${String(minutes).padStart(2,"0")}:${String(seconds).padStart(2,"0")}`;
+}
+function getUserTime(){
+    SavingUserTime();
+    displayUserTime();
+}
+getUserTime();
+setInterval(getUserTime,1000);
