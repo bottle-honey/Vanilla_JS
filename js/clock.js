f8af4645443cc84155699cb692dae381a9883b08
bottle-honey <honey_sheep@naver.com>
2022-03-17 18:32:46 +0900

    #5.2
    PadStar
    문자열 표현방식

--- js/clock.js
+++ js/clock.js
@@ -3,7 +3,10 @@ const clock = document.querySelector("#clock");
 
 function CurrentTime(){
     const date = new Date();
-    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    const time = `${hours}:${minutes}:${seconds}`;
     clock.innerText = time;
 }
 //setInterval : 매 ms 간격으로 함수 실행
