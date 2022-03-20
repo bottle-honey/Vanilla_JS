469480b287f28a23de214a15d3bda6e1eda82816
bottle_honey <honey_sheep@naver.com>
2022-03-20 23:43:38 +0900

    # end
    needs css updated

--- js/background.js
+++ js/background.js
@@ -1,13 +1,16 @@
-const images = [
-    "0.jpg",
-    "1.jpg",
-    "2.jpg"
-];
+const IMAGES_LEN = 9;
+let images = [];
+for(var i =0;i< IMAGES_LEN;i++){
+    images.push(`${i}.jpg`);
+}
 
-const ChosenImage = images[Math.floor(Math.random()*images.length)];
 
-const bgImage = document.createElement("img");
+const ChosenImage = images[Math.floor(Math.random()*IMAGES_LEN)];
 
-bgImage.src = `img/${ChosenImage}`;
+// const bgImage = document.createElement("img");
 
-document.body.appendChild(bgImage);
+// bgImage.src = `img/${ChosenImage}`;
+const imageUrl = `img/${ChosenImage}`;
+
+document.body.style.backgroundImage = "url('" + imageUrl + "')";
+// document.body.appendChild(bgImage);
--- js/weather.js
+++ js/weather.js
@@ -2,10 +2,15 @@ const API_KEY ="d7de59c5efe4fbe800a1cad42192d482";
 
 function onGeoOK(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&Wappid=${API_KEY}`;
-    
-    console.log(url);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weatherContainer = document.querySelector("#weather span:first-child");
+        const cityContainer = document.querySelector("#weather span:last-child");
+        cityContainer.innerText = data.name;
+        weatherContainer.innerText = `현재 온도 : ${data.main.temp} 날씨 : ${data.weather[0].main}`;
+
+    });
 }
 function onGeoError(){
     alert("can't find you.");
